495804dce0a0430d872ae67625120c52bd8a318f
Darach Gorham <darachg22@gmail.com>
2023-02-22 12:43:38 +0000

    test

--- Week04-flow/collatz.py
+++ Week04-flow/collatz.py
@@ -1,3 +1,10 @@
+#Author: Darach Gorham
+
+#asks the user to input any positive integer and outputs the successive values of the following calculation:
+#At each step calculate the next value by taking the current value and, if it is even, divide it by two, but if it is odd, multiply it by three and add one. Have the program end if the current value is one.
+
+#First attempt using no outside research:
+
 #from ast import While
 #num = int(input("Enter a number:"))
 #while num == 1: 
@@ -5,7 +12,7 @@
         #print (f"{num/2}")
     #else:
         #print (f"{num * 3 + 1}")
-#https://stackoverflow.com/questions/63795905/building-a-function-to-return-integer-until-reaching-1-in-python
+#Resource used to help finish assignment: https://stackoverflow.com/questions/63795905/building-a-function-to-return-integer-until-reaching-1-in-python
 
 
 def Collatz(n):
