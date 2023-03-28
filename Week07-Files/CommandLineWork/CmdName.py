2d7e35190f8311ee929d318df1f2bda66dab3366
Darach Gorham <darachg22@gmail.com>
2023-03-28 15:56:50 +0100

    test

---
+++ Week07-Files/CommandLineWork/CmdName.py
@@ -0,0 +1,7 @@
+import sys
+
+#Set up command line argument. Used this to learn and practice: https://machinelearningmastery.com/command-line-arguments-for-your-python-script/ 
+name = sys.argv[1]
+
+print (name)
+
--- Week07-Files/es.py
+++ Week07-Files/es.py
@@ -4,6 +4,8 @@ import sys
 filename = sys.argv[1]
 
 #The section that reads the file. Research needed to find solution to 'UnicodeDecodeError'. Solution found here: https://sites.pitt.edu/~naraehan/python3/reading_writing_methods.html
+#Research and practice led me to the 'as': https://www.w3schools.com/python/ref_keyword_as.asp#:~:text=The%20as%20keyword%20is%20used,using%20c%20instead%20of%20calendar%20. 
+
 with open(filename, "r", encoding="utf-8") as file:
     content = file.read()
     count = content.count("e")
